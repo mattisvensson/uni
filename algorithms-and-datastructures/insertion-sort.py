e0ded79de3c4990f1743b7e5517ba208bab0557e
Matti Svensson <matti.svensson@ueberbit.de>
2024-10-31 09:34:27 +0100

    update insertion sort to use measure util

--- algorithms-and-datastructures/insertion-sort.py
+++ algorithms-and-datastructures/insertion-sort.py
@@ -1,5 +1,4 @@
-import random
-import timeit
+from utils.measure_time import measure
 
 def insertion_sort(arr):
   for i in range(1, len(arr)):
@@ -10,13 +9,4 @@ def insertion_sort(arr):
         arr[j - 1] = tmp      
   return arr
 
-def measure_time(arr_length):
-  arr = list(range(1, arr_length + 1))
-  random.shuffle(arr)
-  insertion_sort(arr)
-
-array_lengths = [100, 200, 1000, 2000, 4000, 10000, 20000, 40000]
-
-for length in array_lengths:
-  execution_time = timeit.timeit(lambda: measure_time(length), number=1)
-  print(f"Execution time for array length {length} over 1 run: {execution_time} seconds")
+measure(insertion_sort, 1)
